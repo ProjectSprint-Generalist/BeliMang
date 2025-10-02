b10c27bbd707c2bec31e785cfeb465ea0646fb2d
Mona-kecil <flowyourminds@gmail.com>
2025-10-02 16:08:35 +0700

    create DTO for create merchant request

---
+++ internal/dto/merchant.go
@@ -0,0 +1,28 @@
+package dto
+
+type MerchantCategory string
+
+const (
+	SmallRestaurant       MerchantCategory = "SmallRestaurant"
+	MediumRestaurant      MerchantCategory = "MediumRestaurant"
+	LargeRestaurant       MerchantCategory = "LargeRestaurant"
+	MerchandiseRestaurant MerchantCategory = "MerchandiseRestaurant"
+	BoothKiosk            MerchantCategory = "BoothKiosk"
+	ConvenienceStore      MerchantCategory = "ConvenienceStore"
+)
+
+type Location struct {
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"long"`
+}
+
+type MerchantCreateRequest struct {
+	Name             string           `json:"name" binding:"required,min=2,max=30"`
+	MerchantCategory MerchantCategory `json:"merchantCategory" binding:"required"`
+	ImageURL         string           `json:"imageURL" binding:"required"`
+	Location         Location         `json:"location" binding:"required"`
+}
+
+type MerchantCreateResponse struct {
+	MerchantID string `json:"merchantID"`
+}
