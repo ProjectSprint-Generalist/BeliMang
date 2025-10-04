c9ab00c5370f10ac688b307c08d25502e894d042
Mona-kecil <flowyourminds@gmail.com>
2025-10-04 17:21:52 +0700

    😭

--- migrations/002_add_merchant_image.up.sql
+++ migrations/002_add_merchant_image.up.sql
@@ -1,2 +1,2 @@
 ALTER TABLE merchants ADD COLUMN image_url TEXT NOT NULL;
-ALTER TABLE merchants ADD CONSTRAINT fk_merchants_images FOREIGN KEY (image_url) REFERENCES images(object_key);
+ALTER TABLE merchants ADD CONSTRAINT fk_merchants_images FOREIGN KEY (image_url) REFERENCES images(url);
